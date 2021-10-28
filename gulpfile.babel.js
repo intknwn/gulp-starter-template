2a6c0cdab4240e5561484a214ca209ee39b8a72c
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2021-10-29 00:06:53 +0300

    Добавляет del

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,13 @@
+import { series } from 'gulp';
+import clean from './gulp/tasks/clean';
 import config from './gulp/config';
 
 config.setEnv();
+
+export const build = series(
+  clean,
+);
+
+export const watch = series(
+  build,
+);
