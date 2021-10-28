b54ac760ca1bcda4245a44b2f9feb494ddb33097
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2021-10-28 21:04:53 +0300

    Добавляет хуки husky

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,3 @@
 import config from './gulp/config';
 
 config.setEnv();
-
-export const build = () => {
-  console.log(config.isProd);
-};
-
-export const watch = () => {
-  console.log(config.isProd);
-};
