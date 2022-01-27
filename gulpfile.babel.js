719aeb379d57a41443c107b65ef42407d2ec2b4f
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-01-27 11:55:57 +0300

    Добавляет зависимости и таски для работы со спрайтами

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,16 +7,24 @@ import { pugBuild, pugWatch } from './gulp/tasks/pug';
 import { sassBuild, sassWatch } from './gulp/tasks/styles';
 import { assetsBuild, assetsWatch } from './gulp/tasks/assets';
 import { imagesBuild, imagesWatch } from './gulp/tasks/images';
+import { spritesBuild, spritesWatch } from './gulp/tasks/sprites';
 
 config.setEnv();
 
 export const build = series(
   clean,
-  parallel(scripts, pugBuild, sassBuild, assetsBuild, imagesBuild),
+  parallel(
+    scripts,
+    pugBuild,
+    sassBuild,
+    assetsBuild,
+    imagesBuild,
+    spritesBuild,
+  ),
 );
 
 export const watch = series(
   build,
   server,
-  parallel(pugWatch, sassWatch, assetsWatch, imagesWatch),
+  parallel(pugWatch, sassWatch, assetsWatch, imagesWatch, spritesWatch),
 );
