2b1fddebc65625e936ba07b8fa2620d11feffc87
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-01-27 10:30:53 +0300

    Добавляет в зависимости необходимые пакеты для работы со стилями

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import sassGlob from 'gulp-sass-glob';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import gulpif from 'gulp-if';
+import autoprefixer from 'gulp-autoprefixer';
+import groupMediaQueries from 'gulp-group-css-media-queries';
+import cleanCSS from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import config from '../config';
+
+const sass = gulpSass(dartSass);
+
+export const sassBuild = (done) => {
+  gulp
+    .src(`${config.src.sass}/main.scss`, { sourcemaps: config.isDev })
+    .pipe(plumber())
+    .pipe(sassGlob())
+    .pipe(
+      sass({
+        includePaths: ['./node_modules'],
+      }),
+    )
+    .pipe(gulpif(config.isProd, groupMediaQueries()))
+    .pipe(gulpif(config.isProd, autoprefixer()))
+    .pipe(gulpif(config.isProd, cleanCSS({ level: 2 })))
+    .pipe(
+      rename({
+        suffix: '.min',
+      }),
+    )
+    .pipe(gulp.dest(config.dest.css, { sourcemaps: config.isDev }));
+
+  done();
+};
+
+export const sassWatch = (done) => {
+  gulp.watch(`${config.src.sass}/**/*.scss`, sassBuild);
+
+  done();
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,10 @@ import server from './gulp/tasks/server';
 import scripts from './gulp/tasks/scripts';
 import config from './gulp/config';
 import { pugBuild, pugWatch } from './gulp/tasks/pug';
+import { sassBuild, sassWatch } from './gulp/tasks/styles';
 
 config.setEnv();
 
-export const build = series(clean, parallel(scripts, pugBuild));
+export const build = series(clean, parallel(scripts, pugBuild, sassBuild));
 
-export const watch = series(build, server, pugWatch);
+export const watch = series(build, server, pugWatch, sassWatch);
